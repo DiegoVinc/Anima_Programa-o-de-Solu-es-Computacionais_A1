6f4e98b86ba1bd4c997d2b5a3ecc0b0df2b99cd0
DIEGO VINICIUS DE SOUSA AZEVEDO <diego.v.azevedo@una.br>
2023-05-09 13:46:05 -0300

    second commit

--- src/App.java
+++ src/App.java
@@ -5,26 +5,26 @@ public class App {
         Scanner input = new Scanner(System.in);
         System.out.print("Digite Seu nome: ");
          String nome = input.next();
-        System.out.print("Digite seu peso atual: ");
+        System.out.print("Digite seu peso atual em Kg: ");
          double peso = input.nextDouble();
         System.out.print("Digite a quantidade de água já ingerida em L: ");
          double aguaIngerida = input.nextDouble();
          input.close();
          double valorMlPorKg = 0.035;
-         double quantidadeIdeal = (valorMlPorKg * peso);
+         double quantidadeIdeal = Math.ceil(valorMlPorKg * peso);
          double quantidadeAIngerir = (quantidadeIdeal - aguaIngerida);
          
 
 
          System.out.println("Nome: " + nome);
          System.out.println("Seu peso Atual é: " + peso);
-         System.out.printf("Você já ingeriu %fL de água hoje.", aguaIngerida);
-         System.out.printf("\nA quantidade ideal de água a ingerir é: %fL", quantidadeIdeal);
-         if (quantidadeAIngerir < 0) {
+         System.out.printf("Você já ingeriu %.0fL de água hoje.", aguaIngerida);
+         System.out.printf("\nA quantidade ideal de água a ingerir é: %.0fL", quantidadeIdeal);
+         if (quantidadeAIngerir <= 0) {
             System.out.println("\nParabéns, você atingiu a meta de hidratação diária");
          }
          else {
-            System.out.printf("\nContinue focado em se hidratar por hoje, ainda faltam [%f] litros! Você consegue!", quantidadeAIngerir);
+            System.out.printf("\nContinue focado em se hidratar por hoje, ainda faltam [%.0f] litros! Você consegue!", quantidadeAIngerir);
          }
          
          
